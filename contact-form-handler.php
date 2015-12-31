949f15d6b03d476f16f2a1e02868854436c39dfc
Zachary Murphy <murphyz@my.wccc.edu>
2015-12-30 20:31:28 -0500

    revisions

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,9 +1,7 @@
 <?php
 // this is the code for the form that will be implemented on the contact page
-
-
 $errors = '';
-$myemail = 'ztmurphy21@gmail.com';//<-- Currently using a test email address, this won't actually do anything... 
+$myemail = 'ztmurphy21@gmail.com';//<-- Currently using a test email address, this won't actually do anything...
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['message']))
@@ -13,32 +11,19 @@ if(empty($_POST['name'])  ||
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
+if(filter_var($email_address, FILTER_VALIDATE_EMAIL)){ // Use php filter_var
+    $errors .= "<br/> Error: Invalid email address";
 }
-
-if( empty($errors))
-{
+if($errors != ''){
+    die($errors."<br/><a href='javascript:history.back(1);'>Back</a>"); //Use die if there are errors to just bail and echo. Only good for this simple text stuff
+}
 $to = $myemail;
 $email_subject = "Contact form submission: $name";
 $email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Message \n $message";
+  " Here are the details:\n Name: $name \n ".
+  "Email: $email_address\n Message \n $message";
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-//redirect to the 'thank you' page
-if (isset($_POST['submitform']))
-    {   
-    ?>
-<script type="text/javascript">
-window.location = "contact-form-thank-you.html";
-</script>      
-    <?php
-    }
-}
-
+mail($to, $email_subject, $email_body, $headers);
+header('Location: http://'.$_SERVER['HTTP_HOST'].'/contact-form-thank-you.html');
 ?>
